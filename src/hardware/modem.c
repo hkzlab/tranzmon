62c721613d3e6e96421b3bc635a7c847356d38bd
Fabio Battaglia <hkzlabnet@gmail.com>
2021-01-28 15:46:08 +0100

    Modem registers

--- src/hardware/modem.c
+++ src/hardware/modem.c
@@ -4,6 +4,12 @@
 #define MODEM_PORT_READ 0x50
 #define MODEM_PORT_WRITE 0x60
 
+#define REG_CR0  0x00
+#define REG_CR1  0x01
+#define REG_DR   0x02
+#define REG_TR   0x03
+#define REG_ID   0x06 
+
 // To read from a modem register, first write the register address into port 0x40
 // Then read the status from port 0x50 or write the value into port 0x60
 
